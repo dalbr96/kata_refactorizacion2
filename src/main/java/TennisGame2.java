69a0e94042081953d70cb4f91f2dc4efe4dfad68
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-18 09:39:34 -0500

    Replace repeated code with getPartialScore() method

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -38,40 +38,16 @@ public class TennisGame2 implements TennisGame
         }
         if (P2point > 0 && P1point==0)
         {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
+            score = this.getPartialScore();
         }
         
         if (P1point>P2point && P1point < 4)
         {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
+            score = this.getPartialScore();
         }
         if (P2point>P1point && P2point < 4)
         {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
+            score = this.getPartialScore();
         }
         
         if (P1point > P2point && P2point >= 3)
